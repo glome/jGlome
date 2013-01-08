ca6c428514c45796a8fe39a7221fbccffdaa7d6f
Arttu Manninen <arttu@kaktus.cc>
2013-01-08 09:45:44 +0200

    Working on new layout

---
+++ js/jquery.glome_template.js
@@ -0,0 +1,34 @@
+/**
+ * This file is just to provide some rudimentary clickable functionality to
+ * Glome template
+ */
+jQuery(function()
+{
+  // Set the default view
+  if (!window.location.hash.match(/#.+/))
+  {
+    window.location.hash = '#admin-subscriptions';
+  }
+  
+  jQuery(window)
+    .on('hashchange', function()
+    {
+      var hash = window.location.hash.toString().replace(/#/, '');
+      var context = (hash.match(/admin/)) ? 'admin' : 'window';
+      var page = hash.replace(/(admin|ad)-/, '');
+      
+      // Toggle main context
+      jQuery('[data-context="' + context + '"]').siblings().addClass('hidden');
+      jQuery('[data-context="' + context + '"]').removeClass('hidden');
+      
+      // Toggle context page
+      jQuery('[data-context="' + context + '"]').find('> .glome-full-width > .glome-content').not('[data-template="' + hash + '"]').addClass('hidden');
+      jQuery('[data-context="' + context + '"]').find('> .glome-full-width > .glome-content').filter('[data-template="' + hash + '"]').removeClass('hidden');
+      
+      // Toggle navigation selected item
+      jQuery('[data-context="' + context + '"]').find('> .glome-header').find('[data-page="' + page + '"]').addClass('selected');
+      jQuery('[data-context="' + context + '"]').find('> .glome-header').find('[data-page="' + page + '"]').siblings().removeClass('selected');
+    })
+    .trigger('hashchange');
+  
+});
