9f85471303249b137d3b9caf755995d1a86500d2
adrenalin <arttu@kaktus.cc>
2013-01-10 15:36:26 +0200

    Working on new Glome layouts

--- js/jquery.glome_template.js
+++ js/jquery.glome_template.js
@@ -10,6 +10,19 @@ jQuery(function()
     window.location.hash = '#admin-subscriptions';
   }
   
+  jQuery('.glome-button[data-state]')
+    .on('click', function(e)
+    {
+      if (jQuery(this).attr('data-state') === 'on')
+      {
+        jQuery(this).attr('data-state', 'off');
+      }
+      else
+      {
+        jQuery(this).attr('data-state', 'on');
+      }
+    });
+  
   jQuery(window)
     .on('hashchange', function()
     {
@@ -22,8 +35,8 @@ jQuery(function()
       jQuery('[data-context="' + context + '"]').removeClass('hidden');
       
       // Toggle context page
-      jQuery('[data-context="' + context + '"]').find('> .glome-full-width > .glome-content').not('[data-template="' + hash + '"]').addClass('hidden');
-      jQuery('[data-context="' + context + '"]').find('> .glome-full-width > .glome-content').filter('[data-template="' + hash + '"]').removeClass('hidden');
+      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent > .glome-full-width > .glome-content').not('[data-template="' + hash + '"]').addClass('hidden');
+      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent > .glome-full-width > .glome-content').filter('[data-template="' + hash + '"]').removeClass('hidden');
       
       // Toggle navigation selected item
       jQuery('[data-context="' + context + '"]').find('> .glome-header').find('[data-page="' + page + '"]').addClass('selected');
