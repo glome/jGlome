2282d2a7ee9d90e80773c035b8f8f3f0aef19109
adrenalin <arttu@kaktus.cc>
2013-01-15 01:40:37 +0200

    Rough sketches of category, category list

--- js/jquery.glome_template.js
+++ js/jquery.glome_template.js
@@ -9,16 +9,82 @@ jQuery(function()
   // Set the default view
   if (!window.location.hash.match(/#.+/))
   {
-    window.location.hash = '#admin-subscriptions';
+    window.location.hash = '#public-startup';
+  }
+  
+  var select = jQuery('<select />')
+    .on('change', function()
+    {
+      window.location.hash = '#' + jQuery(this).val();
+    })
+    .attr('id', 'glomeTemplateSwitch')
+    .prependTo('body')
+    .css
+    (
+      {
+        position: 'absolute',
+        top: '10px',
+        left: '10px',
+        zIndex: 20000
+      }
+    );
+  
+  var pages = jQuery('[data-page]');
+  
+  for (var i = 0; i < pages.size(); i++)
+  {
+    var page = pages.eq(i).attr('data-page');
+    var context = pages.eq(i).parents('[data-glome-container]').attr('data-context');
+    
+    var option = jQuery('<option />')
+      .attr('value', context + '-' + page)
+      .text(context + ': ' + page)
+      .appendTo(select);
   }
   
   jQuery('#glomeAdminContent').find('[data-template="admin-subscriptions"] .glome-row').eq(0).cloneTimes(15);
   jQuery('#glomeAdminContent').find('[data-template="admin-rewards"] .glome-row').eq(0).cloneTimes(15);
   jQuery('#glomePublicContent').find('[data-template="public-subscriptions"] .glome-row').eq(0).cloneTimes(15);
+  jQuery('#glomePublicContent').find('[data-template="public-category"] .glome-row').eq(0).cloneTimes(15);
+  jQuery('#glomePublicContent').find('[data-template="public-categorylist"] .glome-row').eq(0).cloneTimes(15);
+  
+  jQuery('.glome-close')
+    .on('click', function()
+    {
+      window.location.hash = '#widget';
+    });
+  
+  jQuery('#glomePublicFinish').find('button')
+    .on('click', function()
+    {
+      window.location.hash = '#widget-widget';
+    });
+  
+  jQuery('.glome-pager button')
+    .on('click', function()
+    {
+      var context = jQuery(this).parents('[data-glome-container]').attr('data-context');
+      
+      if (jQuery(this).hasClass('left'))
+      {
+        var page = jQuery(this).parents('[data-page]').prev().attr('data-page');
+      }
+      else
+      {
+        var page = jQuery(this).parents('[data-page]').next().attr('data-page');
+      }
+      
+      window.location.hash = '#' + context + '-' + page;
+    });
   
   jQuery('.glome-button[data-state]')
     .on('click', function(e)
     {
+      if (jQuery(this).attr('data-state-change') === 'false')
+      {
+        return true;
+      }
+      
       if (jQuery(this).attr('data-state') === 'on')
       {
         jQuery(this).attr('data-state', 'off');
@@ -48,20 +114,43 @@ jQuery(function()
   
   jQuery('[data-template="admin-subscriptions"], [data-template="public-subscriptions"]').find('.glome-button[data-state]:first').trigger('click');
   
+  jQuery('#glomeWidget')
+    .on('click', function()
+    {
+      if (jQuery(this).attr('data-state') == 'knock')
+      {
+        jQuery(this).attr('data-state', 'open');
+      }
+      else
+      {
+        jQuery(this).attr('data-state', 'knock');
+      }
+    });
+  
   jQuery(window)
     .on('hashchange', function()
     {
       var hash = window.location.hash.toString().replace(/#/, '');
-      var context = (hash.match(/admin/)) ? 'admin' : 'window';
-      var page = hash.replace(/(admin|ad)-/, '');
+      var regs = hash.match(/(admin|public|widget)/);
+      
+      if (!regs)
+      {
+        return;
+      }
+      
+      var regexp = new RegExp(regs[1] + '-');
+      var context = regs[1];
+      var page = hash.replace(regexp, '');
+      
+      jQuery('#glomeTemplateSwitch').find('option[value="' + context + '-' + page + '"]').attr('selected', 'selected').siblings().removeAttr('selected');
       
       // Toggle main context
       jQuery('[data-context="' + context + '"]').siblings().addClass('hidden');
       jQuery('[data-context="' + context + '"]').removeClass('hidden');
       
       // Toggle context page
-      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent, #glomePublicContent').find('[data-context="glome-content-area"] > .glome-content').not('[data-template="' + hash + '"]').addClass('hidden');
-      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent, #glomePublicContent').find('[data-context="glome-content-area"] > .glome-content').filter('[data-template="' + hash + '"]').removeClass('hidden');
+      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent, #glomePublicContent, #glomeWidgetContent').find('[data-context="glome-content-area"] > .glome-content').not('[data-template="' + hash + '"]').addClass('hidden');
+      jQuery('[data-context="' + context + '"]').find('#glomeAdminContent, #glomePublicContent, #glomeWidgetContent').find('[data-context="glome-content-area"] > .glome-content').filter('[data-template="' + hash + '"]').removeClass('hidden');
       
       // Toggle navigation selected item
       jQuery('[data-context="' + context + '"]').find('> .glome-header').find('[data-page="' + page + '"]').addClass('selected');
